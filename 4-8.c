73b5b15fbfd893f47b943d9251ab94f8ea0b8079
John Deschler <jdeschler@college.harvard.edu>
2016-07-21 13:42:28 +0000

    added the A stuff

---
+++ 4-8.c
@@ -0,0 +1,25 @@
+/********
+ * CS50 AP
+ * Code Examples
+ * Module 4-8: Version Control
+ * Version 0: The Starter Code
+*********/
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // Print a diamond with user entered height!
+    printf("Height: ");
+    int height = GetInt();
+    
+    for(int i = 0; i < height / 2; i++)
+    {
+        
+    }
+    for (int i = 0; i < height / 2; i++)
+    {
+        
+    }
+}
